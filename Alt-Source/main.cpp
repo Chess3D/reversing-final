47b62ae31117d72a9006f69d570ba2bfdcd1b242
Chess3D <bennettacarley@gmail.com>
2022-04-18 17:14:32 -0500

    Replace old control_loop with the new code

--- Alt-Source/main.cpp
+++ Alt-Source/main.cpp
@@ -1,11 +1,16 @@
 // External imports
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <sys/time.h>
 #include <string.h>
 #include <map>
 
 // Internal imports
+#include <Alarm.h>
+#include <Entry.h>
+#include <Camera.h>
+#include <Asset.h>
 
 // string namespace
 using std::string;
@@ -18,11 +23,30 @@ using std::endl;
 // fstream namespace
 using std::ofstream;
 
+// sstream namespace
+using std::istringstream;
+
 // Global Variables :(
 struct timeval tv;
 string admin_user = "NEDRY";
 string admin_password = "Mr. Goodbytes";
 bool loggedin = false;
+
+
+// Visitor controls
+Alarm visitorAlarm;
+Entry visitorDoor;
+Camera visitorCamera;
+
+// Park controls 
+Alarm parkAlarm;
+Entry parkDoor;
+Camera parkCamera;
+
+// Lab controls
+Alarm labAlarm;
+Entry vaultDoor;
+Camera vaultCamera;
 
 
 // TODO:  Has an unused input in disassembly (why?)
@@ -43,7 +67,7 @@ bool validate(string &username, string &password)
 {
 
     // TODO:  Password comparision done differently in source code
-    if (!strcmp(admin_user.c_str(), username.c_str()) && !strcmp(admin_password.c_str(), password.c_str()))
+    if ((admin_user == username) && (admin_password == password))
     {
         loggedin = true;
     }
@@ -63,20 +87,17 @@ void visitor_controls()
 
 
 // TODO:  Write function
-void park_controls(ofstream &log)
-{
-
+void park_controls(ofstream &log) {
     int input;
-    do
-    {
+
+    do {
         // Print help menu
         cout << "[1] - Alarm System \n[2] - Entry Controls \n[3] - Car Tracking \n[4] - Inventory \n[0] - QUIT\n";
 
         cin >> input;
         log << input << endl;
 
-        switch (input)
-        {
+        switch (input) {
         case 1:
             // Alarm::printMessage((Alarm *)parkAlarm);
             break;
@@ -96,9 +117,9 @@ void park_controls(ofstream &log)
             carCords["CAR003"] = std::make_pair(10, -10);
             carCords["CAR004"] = std::make_pair(10, 10);
             cout << "=====================\n CURRENT COORDINATES \n=====================\n\n";
+
             auto iter = carCords.begin();
-            while (iter != carCords.end())
-            {
+            while (iter != carCords.end()) {
                 cout << iter->first << "\n=====\n"
                      << iter->second.first << ", " << iter->second.second << "\n\n";
                 ++iter;
@@ -111,8 +132,8 @@ void park_controls(ofstream &log)
             carInfo["CAR004"] = std::make_pair(7, 16);
             cout << "========================================\n CURRENT Passenger Count and Fuel Level \n========================================\n\n";
             iter = carInfo.begin();
-            while (iter != carInfo.end())
-            {
+
+            while (iter != carInfo.end()) {
                 cout << iter->first << "\n=====\n"
                      << "Passengers: "
                      << iter->second.first << "\nFuel Level: " << iter->second.second << "\n\n";
@@ -126,15 +147,14 @@ void park_controls(ofstream &log)
             carInt["CAR004"] = 32;
             cout << "==============================\n CURRENT Structural Integrity \n==============================\n\n";
             auto iter2 = carInt.begin();
-            while (iter2 != carInt.end())
-            {
+            while (iter2 != carInt.end()) {
                 cout << iter2->first << "\n=====\n"
                      << iter2->second << "\n\n";
                 ++iter2;
             }
             cout << "=====================\n\n\n";
         }
-        break;
+            break;
         case 4:
             // Asset a1("Dilophosaurus", 23.0, 4.0);
             // Asset a2("Dilophosaurus", 21.0, 16.0);
@@ -163,10 +183,9 @@ void park_controls(ofstream &log)
 }
 
 
-// TODO:  Write function
-void lab_controls()
-{
-    cout << "LAN CONTROLS" << endl;
+// Prints valid lab controls
+void lab_controls() {
+    cout << "LAB CONTROLS" << endl;
     cout << "alarmstatus --lab\n";
     cout << "unlockdoor --lab\n";
     cout << "lockdoor --lab\n";
@@ -176,30 +195,28 @@ void lab_controls()
 
 
 // This was originally in menus but was moved here to make testing easier
-void check_alarms()
-{
+void check_alarms() {
     cout << "Checking alarms..." << endl;
 
     cout << "Lab:" << endl;
-    // Alarm::printMessage((Alarm *)&labAlarm);
+    labAlarm.printMessage();
 
     cout << "Visitor Center:" << endl;
-    // Alarm::printMessage((Alarm *)&visitorAlarm);
+    visitorAlarm.printMessage();
 
     cout << "Park:" << endl;
-    // Alarm::printMessage((Alarm *)&parkAlarm);
+    parkAlarm.printMessage();
 }
 
 // Function changes
 // 1. Moved the cin into the loop
 // 2. Changed to do-while loop
 // This prevents the code from being stuck in an infinite loop
-void menus(ofstream &log)
-{
+void menus(ofstream &log) {
     int input;
     cout << "Enter HELP at any time to see menus and QUIT to end program" << endl;
-    do
-    {
+
+    do {
         // Print help menu
         cout << "[1] - Visitor Center\n";
         cout << "[2] - Park \n";
@@ -210,44 +227,39 @@ void menus(ofstream &log)
         cin >> input;
         log << input << endl;
 
-        switch (input)
-        {
-        case 1:
-            visitor_controls();
-            break;
-        case 2:
-            park_controls(log);
-            break;
-        case 3:
-            lab_controls();
-            break;
-        case 99:
-            // This repaces the code previously here to help with debuging
-            check_alarms();
-            break;
-        case 0:
-            break;
-        default:
-            cout << "Invalid instruction.\n";
-            break;
+        switch (input) {
+            case 1:
+                visitor_controls();
+                break;
+            case 2:
+                park_controls(log);
+                break;
+            case 3:
+                lab_controls();
+                break;
+            case 99:
+                // This repaces the code previously here to help with debuging
+                check_alarms();
+                break;
+            case 0:
+                break;
+            default:
+                cout << "Invalid instruction.\n";
+                break;
         }
     } while (input);
 }
 
 
-void getGenes()
-{
+void getGenes() {
     cout << "Gene Sequence:" << endl;
     cout << "===================================================" << endl;
 
     char base[4] = {'T', 'G', 'C', 'A'};
 
-    for (int i = 0; i < 200; ++i)
-    {
-        for (int j = 0; j < 5; ++j)
-        {
-            for (int k = 0; k < 10; ++k)
-            {
+    for (int i = 0; i < 200; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            for (int k = 0; k < 10; ++k) {
                 cout << base[rand() % 4];
             }
             cout << "\t";
@@ -258,88 +270,76 @@ void getGenes()
 }
 
 
-// Does not exist in the source code
-// Used to clean up main function
-void control_loop(ofstream &log)
-{
-    string user_input = "";
+// Splits the input into the command and location
+void split_input(string input, string *command, string *location) {
+    string temp;
 
-    while (strcmp("QUIT", user_input.c_str()))
-    {
-        cout << ">> ";
-        cin >> user_input;
-        log << user_input << endl;
+    istringstream ss(input);
 
-        if (!strcmp("HELP", user_input.c_str()))
-        {
-            menus(log);
-        }
-        else
-        {
-            if (!strcmp("QUIT", user_input.c_str()))
-            {
-                return;
-            }
+    ss >> *command;
+    ss >> *location;
+}
 
-            // TODO:  In source code
-            // Remove?  Spaces are not read
-            if (!strcmp("alarmstatus --vc", user_input.c_str()))
-            {
-                // Alarm::printMessage((Alarm *)&visitorAlarm)
-            }
-            else if (!strcmp("unlockdoor --vc", user_input.c_str()))
-            {
-                // Entry::unlock((Entry *)&visitorDoor);
-            }
-            else if (!strcmp("lockdoor --vc", user_input.c_str()))
-            {
-                // Entry::lock((Entry *)&visitorDoor);
-            }
-            else if (!strcmp("camerastatus --vc", user_input.c_str()))
-            {
-                // Camera::displayFeed((Camera *)&visitorCamera);
-            }
-            else if (!strcmp("inventorysummary --vc", user_input.c_str()))
-            {
-                if (!strcmp("alarmstatus --park", user_input.c_str()))
-                {
-                    // Alarm::printMessage((Alarm *)&parkAlarm);
-                }
-                else if (!strcmp("camerastatus --park", user_input.c_str()))
-                {
-                    // parkCameras();
-                }
-                else if (!strcmp("inventorysummary --park", user_input.c_str()))
-                {
-                    if (!strcmp("alarmstatus --lab", user_input.c_str()))
-                    {
-                        // Alarm::printMessage((Alarm *)&labAlarm);
-                    }
-                    else if (!strcmp("unlockdoor --lab", user_input.c_str()))
-                    {
-                        // Entry::unlock((Entry *)&vaultDoor);
-                    }
-                    else if (!strcmp("lockdoor --lab", user_input.c_str()))
-                    {
-                        // Entry::lock((Entry *)&vaultDoor);
-                    }
-                    else if (!strcmp("camerastatus --lab", user_input.c_str()))
-                    {
-                        // Camera::displayFeed((Camera *)&vaultCamera);
-                    }
-                    else if (!strcmp("genesummary --lab", user_input.c_str()))
-                    {
-                        getGenes();
-                    }
-                }
-            }
-        }
+
+// Run the correct command for the given set of systems
+void do_command(string command, Alarm alarm, Entry door, Camera camera) {
+    if (command == "alarmstatus") {
+        alarm.printMessage();
+    } else if (command == "lockdoor") {
+        door.lock();
+    } else if (command == "unlockdoor") {
+        door.unlock();
+    } else if (command == "camerastatus") {
+        camera.displayFeed();
+    } else if (command == "inventorysummary") {
+        cout << "TODO:  Should something happen here?" << endl;
     }
 }
 
 
-int main()
-{
+// The primary control loop of the program
+void control_loop(ofstream &log) {
+    string input;
+
+    // Get the user input
+    cout << ">> ";
+    getline(cin, input);
+
+    // Log the user input
+    log << input << endl;
+
+    // Split the user input into two parts
+    string command;
+    string location;
+
+    split_input(input, &command, &location);
+
+    // Exit when the user types quit
+    if (command == "QUIT") {
+        return;
+    }
+
+    // Run the correct command
+    if (command == "HELP") {
+        menus(log);
+    } else if (location == "--vc") {
+        do_command(command, visitorAlarm, visitorDoor, visitorCamera);
+    } else if (command == "--park") {
+        do_command(command, parkAlarm, parkDoor, parkCamera);
+    } else if (command == "--lab") {
+        if (command == "genesummary") {
+            getGenes();
+        } else {
+            do_command(command, labAlarm, vaultDoor, vaultCamera);
+        }
+    }
+
+    // Continues to loop until "QUIT" is entered
+    control_loop(log);
+}
+
+
+int main() {
     cout << "Welcome to InGen" << endl;
     cout << "Proud leader in Genetics since 1990" << endl
          << endl;
@@ -350,14 +350,12 @@ int main()
     ofstream log("sessionlog.txt");
 
     // Not in original code
-    if (!log.is_open())
-    {
+    if (!log.is_open()) {
         cout << "ERROR:  Log file could not be opened" << endl;
         return 1;
     }
 
-    for (unsigned int i = 0; i < 3; ++i)
-    {
+    for (unsigned int i = 0; i < 3; ++i) {
 
         // Get username
         cout << "User Name:  ";
@@ -379,15 +377,16 @@ int main()
         // Log the log message
         log << logdata();
 
-        if (validate(username, password))
-        {
+        if (validate(username, password)) {
             control_loop(log);
             break;
         }
     }
+
     if (!loggedin) {
         cout << "TOO MANY ATTEMPTS" << endl;
     }
+
     log.close();
 
     return 0;
